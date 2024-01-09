16c74942b7f208ea7dfed76598d16f93694cf389
Luiz Mateus <mateusrobr@gmail.com>
2024-01-08 22:26:08 -0300

    ainnn calica

--- Banco de Dados I/Aula01.py
+++ Banco de Dados I/Aula01.py
@@ -2,9 +2,9 @@ import pandas as pd
 import numpy as np
 import random
 import time
-
+cargos = ['UX', 'DV', 'GER', 'CEO']
 def gerar_registros():
-    cargos = ['UX', 'DV', 'GER', 'CEO', 'TW']
+    #cargos = ['UX', 'DV', 'GER', 'CEO']
     senioridades = ['JR', 'PL', 'SR']
     ids = list(range(1, 40001))
     nomes = ['Nome'+str(i) for i in ids]
@@ -27,13 +27,23 @@ def calcular_custo_de_folha(df):
     return df['Salário'].sum()
 
 def layoff(df):
-    df = df.sort_values(by='Salário', ascending=False)
-    df = df.iloc[12000:]
+    #df = df.sort_values(by='Salário', ascending=False)
+    #df = df.iloc[12000:]
+    
+    for cargo in cargos:
+        df_aux = df.loc[df['Cargo'] == cargo]
+        df_aux = df_aux.sort_values(by='Salário', ascending=False)
+        #print(df_aux)
+        df_aux = df_aux.drop(df_aux.index[3000:len(df_aux.index)])
+        #print(len(df_aux.index))
+        df = df.drop(df_aux.index)
+        
     return df
 
 
 if __name__ == "__main__":
     df = gerar_registros()
+    print(len(df.index))
     start_time = time.time()
     print('Custo de folha antes do layoff: ', calcular_custo_de_folha(df))
     print('Tempo de execução: ', time.time() - start_time)
@@ -43,3 +53,4 @@ if __name__ == "__main__":
     start_time = time.time()
     print('Custo de folha após o layoff: ', calcular_custo_de_folha(df))
     print('Tempo de execução: ', time.time() - start_time)
+    print(len(df.index))
